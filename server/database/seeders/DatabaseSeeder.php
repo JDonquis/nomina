9920d0ed61c3150abdd2a9cdea7e3ff8119be07f
Juan Donquis <juandonquis07@gmail.com>
2026-04-21 16:08:27 -0400

    add directive personnel

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -20,122 +20,132 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        $this->restartDatabase('10abril2026');
-
-        $this->call([
-            TypePersonnelSeeder::class,
+        TypePersonnel::create([
+            'code' => 207,
+            'laboral_relationship' => 'Alto nivel y Dirección',
+            'type_personal' => 'Personal Directivo',
+            'name' => 'Personal Directivo Y Alto Nivel Descentralizado',
+            'source_budget' => 'Mpps - Falcón'
         ]);
 
+        // $this->restartDatabase('20abril2026');
 
-        $allPaySheets = PaySheet::get();
+        // $this->call([
+        //     TypePersonnelSeeder::class,
+        // ]);
 
-        foreach ($allPaySheets as $paySheet) {
-            $paySheetCreatedAt = $paySheet->created_at;
+        // Personnel::truncate();
+        // AuditLog::truncate();
 
-            $typePersonnel = null;
+        // $allPaySheets = PaySheet::get();
 
-            if ($paySheet->typePaySheet) {
-                $typePersonnel = TypePersonnel::where('code', $paySheet->typePaySheet->code)->first();
-            }
+        // foreach ($allPaySheets as $paySheet) {
+        //     $paySheetCreatedAt = $paySheet->created_at;
 
-            $asic = null;
-            if ($paySheet->administrative_location_id) {
-                $asic = ASIC::find($paySheet->administrative_location_id);
-            }
+        //     $typePersonnel = null;
 
-            $additionalData = [];
+        //     if ($paySheet->typePaySheet) {
+        //         $typePersonnel = TypePersonnel::where('code', $paySheet->typePaySheet->code)->first();
+        //     }
 
-            if ($paySheet->type_pension) {
-                $additionalData['type_pension'] = $paySheet->type_pension;
-            }
-            if ($paySheet->last_charge) {
-                $additionalData['last_charge'] = $paySheet->last_charge;
-            }
-            if ($paySheet->minor_child_nro) {
-                $additionalData['minor_child_nro'] = $paySheet->minor_child_nro;
-            }
-            if ($paySheet->disabled_child_nro) {
-                $additionalData['disabled_child_nro'] = $paySheet->disabled_child_nro;
-            }
-            if ($paySheet->another_organization_name) {
-                $additionalData['another_organization_name'] = $paySheet->another_organization_name;
-            }
-            if ($paySheet->fullname_causative) {
-                $additionalData['fullname_causative'] = $paySheet->fullname_causative;
-            }
-            if ($paySheet->age_causative) {
-                $additionalData['age_causative'] = $paySheet->age_causative;
-            }
-            if ($paySheet->parent_causative) {
-                $additionalData['parent_causative'] = $paySheet->parent_causative;
-            }
-            if ($paySheet->sex_causative) {
-                $additionalData['sex_causative'] = $paySheet->sex_causative;
-            }
-            if ($paySheet->ci_causative) {
-                $additionalData['ci_causative'] = $paySheet->ci_causative;
-            }
-            if ($paySheet->decease_date) {
-                $additionalData['decease_date'] = $paySheet->decease_date;
-            }
-            if ($paySheet->last_payment) {
-                $additionalData['last_payment'] = $paySheet->last_payment;
-            }
-            if ($paySheet->user_id) {
-                $additionalData['user_id'] = $paySheet->user_id;
-            }
+        //     $asic = null;
+        //     if ($paySheet->administrative_location_id) {
+        //         $asic = ASIC::find($paySheet->administrative_location_id);
+        //     }
 
-            $personnel = Personnel::create([
-                'status' => 'inactive',
-                'nac' => $paySheet->nac,
-                'ci' => $paySheet->ci,
-                'email' => $paySheet->email,
-                'phone_number' => $paySheet->phone_number,
-                'address' => $paySheet->address,
-                'municipality' => $paySheet->municipality,
-                'parish' => $paySheet->parish,
-                'state' => $paySheet->state,
-                'city' => $paySheet->city,
-                'full_name' => $paySheet->full_name,
-                'date_birth' => $paySheet->date_birth,
-                'sex' => $paySheet->sex,
-                'civil_status' => $paySheet->civil_status,
-                'photo' => $paySheet->photo,
-                'type_personnel_id' => $typePersonnel?->id,
-                'asic_id' => $asic?->id,
-                'receive_pension_from_another_organization_status' => $paySheet->receive_pension_from_another_organization_status,
-                'has_authorizations' => $paySheet->has_authorizations,
-                'pension_survivor_status' => $paySheet->pension_survivor_status,
-                'suspend_payment_status' => $paySheet->suspend_payment_status,
-                'additional_data' => !empty($additionalData) ? $additionalData : null,
-                'census_status' => $paySheet->status
-            ]);
+        //     $additionalData = [];
 
-            $personnel->update([
-                'created_at' => $paySheetCreatedAt,
-                'updated_at' => $paySheetCreatedAt,
-            ]);
+        //     if ($paySheet->type_pension) {
+        //         $additionalData['type_pension'] = $paySheet->type_pension;
+        //     }
+        //     if ($paySheet->last_charge) {
+        //         $additionalData['last_charge'] = $paySheet->last_charge;
+        //     }
+        //     if ($paySheet->minor_child_nro) {
+        //         $additionalData['minor_child_nro'] = $paySheet->minor_child_nro;
+        //     }
+        //     if ($paySheet->disabled_child_nro) {
+        //         $additionalData['disabled_child_nro'] = $paySheet->disabled_child_nro;
+        //     }
+        //     if ($paySheet->another_organization_name) {
+        //         $additionalData['another_organization_name'] = $paySheet->another_organization_name;
+        //     }
+        //     if ($paySheet->fullname_causative) {
+        //         $additionalData['fullname_causative'] = $paySheet->fullname_causative;
+        //     }
+        //     if ($paySheet->age_causative) {
+        //         $additionalData['age_causative'] = $paySheet->age_causative;
+        //     }
+        //     if ($paySheet->parent_causative) {
+        //         $additionalData['parent_causative'] = $paySheet->parent_causative;
+        //     }
+        //     if ($paySheet->sex_causative) {
+        //         $additionalData['sex_causative'] = $paySheet->sex_causative;
+        //     }
+        //     if ($paySheet->ci_causative) {
+        //         $additionalData['ci_causative'] = $paySheet->ci_causative;
+        //     }
+        //     if ($paySheet->decease_date) {
+        //         $additionalData['decease_date'] = $paySheet->decease_date;
+        //     }
+        //     if ($paySheet->last_payment) {
+        //         $additionalData['last_payment'] = $paySheet->last_payment;
+        //     }
+        //     if ($paySheet->user_id) {
+        //         $additionalData['user_id'] = $paySheet->user_id;
+        //     }
 
-            $action = 'create';
+        //     $personnel = Personnel::create([
+        //         'status' => 'inactive',
+        //         'nac' => $paySheet->nac,
+        //         'ci' => $paySheet->ci,
+        //         'email' => $paySheet->email,
+        //         'phone_number' => $paySheet->phone_number,
+        //         'address' => $paySheet->address,
+        //         'municipality' => $paySheet->municipality,
+        //         'parish' => $paySheet->parish,
+        //         'state' => $paySheet->state,
+        //         'city' => $paySheet->city,
+        //         'full_name' => $paySheet->full_name,
+        //         'date_birth' => $paySheet->date_birth,
+        //         'sex' => $paySheet->sex,
+        //         'civil_status' => $paySheet->civil_status,
+        //         'photo' => $paySheet->photo,
+        //         'type_personnel_id' => $typePersonnel?->id,
+        //         'asic_id' => $asic?->id,
+        //         'receive_pension_from_another_organization_status' => $paySheet->receive_pension_from_another_organization_status,
+        //         'has_authorizations' => $paySheet->has_authorizations,
+        //         'pension_survivor_status' => $paySheet->pension_survivor_status,
+        //         'suspend_payment_status' => $paySheet->suspend_payment_status,
+        //         'additional_data' => !empty($additionalData) ? $additionalData : null,
+        //         'census_status' => $paySheet->status
+        //     ]);
 
-            if ($paySheet->status) {
-                $action = 'create_and_census';
-            }
+        //     $personnel->update([
+        //         'created_at' => $paySheetCreatedAt,
+        //         'updated_at' => $paySheetCreatedAt,
+        //     ]);
 
-            $auditLog = AuditLog::create([
-                'action' => $action,
-                'auditable_type' => Personnel::class,
-                'auditable_id' => $personnel->id,
-                'user_id' => $paySheet->user_id ?? 1,
-                'old_values' => null,
-                'new_values' => $personnel->toArray(),
-            ]);
+        //     $action = 'create';
 
-            $auditLog->update([
-                'created_at' => $paySheetCreatedAt,
-                'updated_at' => $paySheetCreatedAt,
-            ]);
-        }
+        //     if ($paySheet->status) {
+        //         $action = 'create_and_census';
+        //     }
+
+        //     $auditLog = AuditLog::create([
+        //         'action' => $action,
+        //         'auditable_type' => Personnel::class,
+        //         'auditable_id' => $personnel->id,
+        //         'user_id' => $paySheet->user_id ?? 1,
+        //         'old_values' => null,
+        //         'new_values' => $personnel->toArray(),
+        //     ]);
+
+        //     $auditLog->update([
+        //         'created_at' => $paySheetCreatedAt,
+        //         'updated_at' => $paySheetCreatedAt,
+        //     ]);
+        // }
     }
 
     private function restartDatabase($filename)
