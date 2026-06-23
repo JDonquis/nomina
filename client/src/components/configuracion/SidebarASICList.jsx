f662d010298c0dcf296489de0660e7aacea15d2c
juanvillans <juanvillans16@gmail.com>
2026-06-23 19:46:09 -0400

    ni

--- client/src/components/configuracion/SidebarASICList.jsx
+++ client/src/components/configuracion/SidebarASICList.jsx
@@ -23,7 +23,26 @@ export default function SidebarASICList({
     <div className="w-80   bg-white border-r border-gray-200 h-96 md:h-full flex flex-col">
       <div className="p-4 border-b border-gray-200 flex items-center justify-between">
         <h2 className="text-lg font-semibold text-gray-800">ASICs</h2>
-      <button onClick={onToggleMap} className="text-sm flex items-center gap-2 p-1 hover:bg-slate-200" > {isMapOpen ? "Ocultar Mapa" : "Ver Mapa"} <Icon icon="mdi:map" /> </button>
+
+
+        <div className="flex items-center gap-2">
+          <button
+                        className=" bg-color2/10 px-2 mr-2  py-0.5 rounded flex text-color2"
+                        title="Cantidad personal activo censado"
+                        onClick={() =>
+                          onGetReportActiveCensus(dependency.id, "Dependencia", { asicName, dependencyName: dependency.name })
+                        }
+                      >
+                        
+                        <Icon
+                          icon="ci:wavy-check"
+                          className="ml-1 text-gray-500"
+                          width={12}
+                          height={12}
+                        />
+                      </button>
+            <button onClick={onToggleMap} className="text-sm flex items-center gap-2 p-1 hover:bg-slate-200" > {isMapOpen ? "Ocultar Mapa" : "Ver Mapa"} <Icon icon="mdi:map" /> </button>
+        </div>
       </div>
 
       
